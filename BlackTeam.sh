4c158e5f38e4b24a86beba829c79108f289e4f7c
Mr-IngsS <54253110+Mr-IngsS@users.noreply.github.com>
2019-08-30 02:36:46 +0700

    New

---
+++ BlackTeam.sh
@@ -0,0 +1,248 @@
+#!/system/xbin/bash
+clear
+red='\033[31;1m'
+blue='\033[34;1m'
+green='\033[32;1m'
+yellow='\033[33;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+sleep 2
+echo
+toilet -f big -F gay BCT
+echo $green"<______________________________________________>"
+echo "\033[34;1m Selamat Datang Di Tools BlackCyberTeam"
+echo "\033[35;1m Author  : kebersamaan
+echo "\033[33;1m My Team : BlackCyberTeam"
+echo "\033[32;1m Massage : Tidak Ada Satupun Sistem Yang Aman"
+echo $green"<---------------------------------------------->"
+echo $purple"BLACKCYBERTEAM"      
+echo $yellow"Silahkan Install Paket Terlebih Dahulu"
+sleep 5
+echo                                                                  
+echo "\033[32;1m SILAHKAN PILIH TOOLS :"
+echo $white"<==============================>"
+echo "\033[31;1m 1.) Spam Telpon"
+sleep 2
+echo $white"<_____________________________>"
+echo "\033[34;1m 2.) Hack Facebook"
+sleep 2
+echo $white"<_____________________________>"
+echo "\033[33;1m 3.) B-VIRUS"
+sleep 2
+echo $white"<_____________________________>"
+echo "\033[35;1m 4.) Email Clone"
+sleep 2
+echo $white"<_____________________________>"
+echo "\033[36;1m 5.) HiddenEye"
+sleep 2
+echo $white"<_____________________________>"     
+echo "\033[34;1m 6.) BLACKCYBERTEAM"
+sleep 2
+echo $white"<_____________________________>"
+echo "\033[31;1m 7.) Install Paket" 
+sleep 2
+echo $white"<_____________________________>"
+sleep 2
+echo "\033[33;1m 8.) RED_HAWK"                                                                      
+sleep 2
+echo $white"<_____________________________>"
+sleep 2
+echo "\033[34;1m 9.) Tool Vip"
+sleep 2
+echo $white"<_____________________________>"
+echo "\033[32;1m 10.) Close Tools"
+sleep 2
+echo "<====================================>"
+echo $white
+read -p "Silahkan Pilih Tools : " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Gooo
+echo $green "Author : H4CKYOUT7H_"
+echo $blue "My Team : BlackCyberTeam"                                                                
+echo $Yellow "Tools : Spam Telpon"
+echo $green"<==========================>"
+sleep 2
+pkg install git
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM                                                                                          
+sh LITESPAM.sh
+sleep 2
+fi
+                                                                                                     
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear                                                                                                
+toilet -f big -F gay Gooo
+echo $red "Author : Mr.IngsS"
+echo $green "My Team : BlackCyberTeam"
+echo $purple "Tools : Hack Facebook"
+echo $yellow"<=========================>"
+sleep 2
+pkg install git
+git clone https://github.com/bukanhacker/Sad.git
+cd Sad
+python2 Sad.py
+sleep 2
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear                                                                                                
+toilet -f big -F gay Gooo
+echo $blue "Author : Mr.ÇØDÊX°"
+echo $yellow "My Team : BlackCyberTeam"
+echo $purple "Tools : B-VIRUS"
+echo $red"<=============================>"
+sleep 2                                                
+pkg update && pkg upgrade                                           
+pkg install git
+pkg install python2
+pkg install pip2
+pip2 install mechanize
+pip2 install requests
+pip2 install tqdm
+git clone https://github.com/BlackCyberAnonim/B-VIRUS
+cd B-VIRUS
+python2 B-VIRUS.py
+sleep 2
+fi
+                                                                                                     
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f big -F gay Gooo
+echo $blue "Author : Clay_Mr_Bott"
+echo $yellow "My Team : BlackCyberTeam"
+echo $red "Tools : Email Clone"
+echo $green"<=============================>
+sleep 2
+pkg install git
+git clone https://github.com/CiKu370/OSIF
+cd OSIF
+python2 osif.py
+sleep 2
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f big -F gay Gooo                                                                            
+echo $yellow" Author : Mr.STYLE"
+echo $green" My Team : BlackCyberTeam"
+echo $purple" Tools : HiddenEye"
+echo $blue"<~~~~~~~~~~~~~~~~~~~~~~~~>
+apt install python git openssh grep php curl
+git clone https://github.com/DarkSecDevelopers/HiddenEye.git
+chmod 777 HiddenEye
+cd HiddenEye
+pip install requirements.txt
+pip install -r requirements.txt
+python HiddenEye.py
+sleep 2
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f big -F gay Gooo
+echo $red" Author : FAMILY-BLACKCYBERTEAM"
+echo $blue"Pesan  : BlackCyberTeam"
+echo $green"<______________________________>"
+sleep 2
+echo
+echo $green" JANGAN PERNAH MEREMEHKAN TEKAT SEORANG ANAK YANG KAYA DENGAN WAKTU DAN MISKIN UANG"
+sleep 2
+echo
+echo $green"<•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°"
+echo $blue" I am MR.INGSS.."
+sleep 2
+echo "<===========================>"
+echo $red" I am MR.FIDSS.."                                                                          
+sleep 2
+echo "<===========================>"
+echo $yellow" I am CLAY_MR_BOOT.."
+sleep 2
+echo "<===========================>"
+echo $green" I am MR.ÇØDÊX°.."                                                                       
+sleep 2
+echo "<===========================>"
+echo $purple" I am H4CKYOU7H_.."
+sleep 2
+echo "<===========================>"
+echo $blue" I am MR.STYLE..."
+sleep 2                                                                                              
+echo "<===========================>"
+echo $yellow" I am MarSha°.."
+sleep 2
+echo $green"<•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°"
+sleep 5
+exit
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet -f big -F gay OTW
+echo" Install Paket Dulu Supaya Lancar"
+pkg update && pkg upgrade
+pkg install python2
+pkg install pip2
+pip2 install mechanize
+pip2 install requests
+pip2 install tqdm
+pkg install php
+pkg install toilet
+pkg install git
+sh cyberteam.sh
+sleep 2
+exit
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet -f big -F gay Attack
+echo $blue" Author : Mr.FidsS"
+echo $yellow" My Team : BlackCyberArmy"
+echo $green" Tools : RED_HAWK"
+echo $red"<|============================|>
+sleep 2
+pkg install php
+pkg install git
+git clone https://github.com/Tuhinshubhra/RED_Hawk
+cd RED_Hawk
+chmod +x rhawk.php
+php rhawk.php
+sleep2
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet -f big -F gay Hobbah
+echo $green" Author : Mr.IngsS"
+echo $blue" My Team : BlackCyberArmy"
+echo $red" Tools : Sadap-Kamera"
+echo $yellow"<|=======================|>
+sleep 2
+apt update && apt upgrade
+apt install openssh
+apt install php
+apt install git
+git clone https://github.com/thelinuxchoice/saycheese.git
+cd saycheese
+bash saycheese.sh
+sleep 2
+fi
+
+if [$bro = 10 ] || [ $bro = 10 ]
+then
+clear
+echo $blue" <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+echo $green" Massage : TERIMA KASIH SUDAH MENGGUNAKAN TOOLS KAMI"     }"
+echo $blue" <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>"
+exit
+fi
+
